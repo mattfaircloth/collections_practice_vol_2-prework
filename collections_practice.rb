a198b24839191f666f0a2b0835c629c5f3046312
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-25 23:01:17 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,10 @@
 # your code goes here
 def begins_with_r(array)
   array.select do |word|
-    array.include?("r")
+    if array.include?("r")
       return true
+    else
+      false
+    end
   end
-  false
 end
